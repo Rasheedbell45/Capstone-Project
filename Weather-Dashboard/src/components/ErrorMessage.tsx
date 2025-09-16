27048ca85b2b6cdf379f3e979367882543e9985e
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-16 11:46:26 +0100

    Update ErrorMessage.tsx

--- Weather-Dashboard/src/components/ErrorMessage.tsx
+++ Weather-Dashboard/src/components/ErrorMessage.tsx
@@ -1,11 +1,11 @@
 interface Props {
-  message: string;
+message: string;
 }
 
 export default function ErrorMessage({ message }: Props) {
-  return (
-    <div className="bg-red-600 text-white p-4 rounded-lg m-4">
-      ❌ {message}
-    </div>
-  );
+return (
+<div className="bg-red-500 text-white text-center p-4 rounded-lg mt-4">
+⚠️ {message}
+</div>
+);
 }
