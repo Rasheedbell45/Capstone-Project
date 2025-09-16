da0ff358a13ddad221498de3c7f080ae69001c35
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-16 07:32:30 +0100

    Update config.ts

--- Weather-Dashboard/src/config.ts
+++ Weather-Dashboard/src/config.ts
@@ -1,2 +1,2 @@
-export const API_KEY = "your_api_key_here";
+export const API_KEY = import.meta.env.VITE_WEATHER_API_KEY as string;
 export const API_URL = "https://api.openweathermap.org/data/2.5";
