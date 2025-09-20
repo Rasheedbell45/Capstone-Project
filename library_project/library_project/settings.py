be4ff412fb805055737b0761bfc044e496543cf4
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-20 15:50:11 +0100

    Update settings.py

--- library_project/library_project/settings.py
+++ library_project/library_project/settings.py
@@ -35,7 +35,12 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    ...
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 STATIC_URL = "/static/"
