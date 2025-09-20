7c591edf040214354efa50b5563d667a06858de0
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-20 20:49:19 +0100

    Update urls.py

--- library_project/library_project/urls.py
+++ library_project/library_project/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+# A simple home view
+def home(request):
+    return HttpResponse("Welcome to the Capstone Project!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('library.urls')),
+    path('api/', include('library.urls')),  # Your existing API
+    path('', home, name='home'),  # Root URL points here
 ]
