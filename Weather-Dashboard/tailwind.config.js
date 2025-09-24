b442c55b5965631913fd6abd9098d25b0f101cbf
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-24 07:59:27 +0100

    Update tailwind.config.js

--- Weather-Dashboard/tailwind.config.js
+++ Weather-Dashboard/tailwind.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'tailwindcss';
-
-export default defineConfig({
+/** @type {import('tailwindcss').Config} */
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: { extend: {} },
   plugins: [],
-});
+};
