654e0fb1a71f64d928f600b4a42671d7a12416a1
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-24 08:00:21 +0100

    Update vite.config.ts

--- Weather-Dashboard/vite.config.ts
+++ Weather-Dashboard/vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
+  server: {
+    port: 5173,
+    open: true
   },
+  build: {
+    outDir: "dist",
+    sourcemap: true
+  }
 });
