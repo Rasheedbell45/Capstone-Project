16b5a62914cd2028677012d5d422df5813bcdc4f
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-25 06:57:36 +0100

    Update weatherService.ts

--- weather_dashboard/src/services/weatherService.ts
+++ weather_dashboard/src/services/weatherService.ts
@@ -2,10 +2,10 @@ import { getCurrentWeather, getForecast } from "../api/weatherApi";
 
 export async function fetchCurrentWeather(city: string) {
   const res = await getCurrentWeather(city);
-  return res; // already returns the weather object
+  return res;
 }
 
 export async function fetchForecast(city: string) {
   const res = await getForecast(city);
-  return res; // already returns the forecast object
+  return res;
 }
