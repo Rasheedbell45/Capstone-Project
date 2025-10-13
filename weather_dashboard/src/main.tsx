f044abd5d75984aa8620360b089e2d48511758b0
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-10-13 11:59:19 +0100

    Update main.tsx

--- weather_dashboard/src/main.tsx
+++ weather_dashboard/src/main.tsx
@@ -17,15 +17,15 @@ createRoot(document.getElementById("root")!).render(
   </React.StrictMode>
 );
 
-// if ('serviceWorker' in navigator) {
-  // window.addEventListener('load', () => {
-   // navigator.serviceWorker
-     // .register('/sw.js')
-      //.then((reg) => {
-        //console.log('Service Worker registered:', reg);
-      //})
-      //.catch((err) => {
-        //console.error('Service Worker registration failed:', err);
-      //});
-  //});
-//}
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+   navigator.serviceWorker
+     .register('/sw.js')
+      .then((reg) => {
+        console.log('Service Worker registered:', reg);
+      })
+      .catch((err) => {
+        console.error('Service Worker registration failed:', err);
+      });
+  });
+}
