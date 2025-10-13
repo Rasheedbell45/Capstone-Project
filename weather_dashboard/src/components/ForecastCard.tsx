dab25c3118e7b6884e9f31346edf0ef0d6bbec08
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-10-13 12:35:48 +0100

    Update ForecastCard.tsx

--- weather_dashboard/src/components/ForecastCard.tsx
+++ weather_dashboard/src/components/ForecastCard.tsx
@@ -1,1 +1,22 @@
+import React from "react";
+import type { ForecastItem } from "../types/weather";
 
+interface ForecastCardProps {
+  data: ForecastItem;
+}
+
+const ForecastCard: React.FC<ForecastCardProps> = ({ data }) => {
+  const date = new Date(data.dt * 1000);
+  const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+  return (
+    <div className="bg-white text-black rounded-2xl shadow-lg p-4 flex flex-col items-center">
+      <p className="font-semibold mb-1">{date.toLocaleDateString(undefined, { weekday: "short" })}</p>
+      <img src={iconUrl} alt={data.weather[0].description} className="w-16 h-16" />
+      <p className="font-bold">{Math.round(data.main.temp)}°C</p>
+      <p className="capitalize text-sm">{data.weather[0].description}</p>
+    </div>
+  );
+};
+
+export default ForecastCard;
