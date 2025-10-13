ec1f68df1192c01489afd1227f17d64614148e00
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-10-13 12:29:46 +0100

    Update weather.ts

--- weather_dashboard/src/types/weather.ts
+++ weather_dashboard/src/types/weather.ts
@@ -1,1 +1,47 @@
+export interface Weather {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+  };
+  sys: {
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  name: string;
+}
 
+export interface ForecastItem {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    humidity: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+}
