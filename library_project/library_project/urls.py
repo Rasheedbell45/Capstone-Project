ee17a1dd6dba3a8d761f74f20ae8b6864ab96c18
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-10-20 12:10:08 +0100

    Update urls.py

--- library_project/library_project/urls.py
+++ library_project/library_project/urls.py
@@ -1,1 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("Welcome to the Library API. Visit /api/ for endpoints.")
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('library.urls')),
+    path('', home, name='home'),  # <-- Root URL
+]
