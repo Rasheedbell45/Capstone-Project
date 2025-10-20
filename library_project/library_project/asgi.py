762d93f3fe1289d75ed7d599d1c38d7a195c1739
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-10-20 12:08:41 +0100

    Update asgi.py

--- library_project/library_project/asgi.py
+++ library_project/library_project/asgi.py
@@ -1,14 +1,1 @@
-"""
-ASGI config for library_project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-This file is used by ASGI servers (like Daphne or Uvicorn) to run async Django apps.
-"""
-
-import os
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "library_project.settings")
-
-application = get_asgi_application()
