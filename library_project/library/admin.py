4729a9f8536e50153809d715e80f38ca506dd747
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-10-20 12:00:59 +0100

    Update admin.py

--- library_project/library/admin.py
+++ library_project/library/admin.py
@@ -1,1 +1,12 @@
+from django.contrib import admin
+from .models import Book, Checkout
 
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "isbn", "available_copies", "total_copies")
+    search_fields = ("title", "author", "isbn")
+
+@admin.register(Checkout)
+class CheckoutAdmin(admin.ModelAdmin):
+    list_display = ("book", "user", "checked_out_at", "due_date", "returned_at")
+    list_filter = ("returned_at",)
