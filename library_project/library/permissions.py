a6b1db19383fc7b7989339a5fe47accb293051a7
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-10-20 12:03:17 +0100

    Update permissions.py

--- library_project/library/permissions.py
+++ library_project/library/permissions.py
@@ -1,10 +1,1 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
 
-class IsAdminOrReadOnly(BasePermission):
-    """
-    Allow read-only for any request, but unsafe methods only for admin users.
-    """
-    def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
-            return True
-        return bool(request.user and request.user.is_staff)
