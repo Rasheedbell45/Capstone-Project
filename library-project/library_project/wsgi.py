c6323aac5a35a61f5541d279f643a7f544eaf542
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-10-21 16:12:40 +0100

    Update wsgi.py

--- library-project/library_project/wsgi.py
+++ library-project/library_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for library_project.
+WSGI config for library-project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -9,6 +9,7 @@ This file is used by WSGI servers (like Gunicorn or uWSGI) to run your project.
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "library_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "library-project.settings")
 
 application = get_wsgi_application()
+
